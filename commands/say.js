6376f7da627f6a278de44028350616cd5157dd5b
Estralia <bluelighty123@gmail.com>
2017-11-26 20:00:26 +0200

    Removed security issue with say command
    Basically if you would've typed ''-say -help'' then the bot would say -help and then it would run the help command. And if you did it with infinite -say -say's then you'd break the server basically.

--- commands/say.js
+++ commands/say.js
@@ -2,6 +2,7 @@ exports.run = function(client, message, args) {
 	if (!message.member.hasPermission("VIEW_AUDIT_LOG")) return message.channel.sendMessage("**:angry: You don't have permission to use this command!**");
 	let suffix = args.join(" ");
 	if(suffix) {
+		if (suffix.startsWith("-")) return message.channel.send("Donkey don't even try to! :angry:");
 		message.channel.send(suffix);
 	} else {
 		message.channel.send("**You have to include something for me to say! :facepalm:**");
