8e05c5dedfb631726a65b8012e10b5080f196731
Estralia <bluelighty123@gmail.com>
2017-11-26 12:48:21 +0200

    Edited say command permissions

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,5 @@
 exports.run = function(client, message, args) {
+	if (!message.member.hasPermission("VIEW_AUDIT_LOG")) return message.channel.sendMessage("**:angry: You don't have permission to use this command!**");
 	let suffix = args.join(" ");
 	if(suffix) {
 		message.channel.send(suffix);
