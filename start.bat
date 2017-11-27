7a4e378a26584e38828f31d4ac66fb8b9aa931d3
Estralia <bluelighty123@gmail.com>
2017-11-27 20:00:40 +0200

    Added some fancy things

--- start.bat
+++ start.bat
@@ -1,11 +1,22 @@
 @echo off
 color 0a
 echo Starting up bot.
-PING localhost -n 6 >NUL
+echo sudo -install DED_WARE
+echo  Done
+PING localhost -n 3 >NUL
 color 0c
 echo Loading modules..
-PING localhost -n 6 >NUL
+PING localhost -n 4 >NUL
 color 09
+PING localhost -n 4 >NUL
+color 0a
+color 0b
+color 0c
+PING localhost -n 4 >NUL
+color 01
+color 02
+color 03
+color 04
 echo FINISHED LOADING!
-PING localhost -n 6 >NUL
+color 09
 node app
