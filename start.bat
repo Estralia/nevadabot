7c0bf6738a56a409ded52262724ac7a8679d9493
Estralia <bluelighty123@gmail.com>
2017-11-27 16:52:09 +0200

    Added starter for bot (optional)
    You can use this to make it look cooler. But you don't have to.

---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+color 0a
+echo Starting up bot.
+PING localhost -n 6 >NUL
+color 0c
+echo Loading modules..
+PING localhost -n 6 >NUL
+color 09
+echo FINISHED LOADING!
+PING localhost -n 6 >NUL
+node app
