d7a66d33638308071af4b2bdc309e129a0dc9b2d
Estralia <bluelighty123@gmail.com>
2017-11-27 19:12:29 +0200

    Added reload function

--- app.js
+++ app.js
@@ -7,4 +7,19 @@ client.on('guildDelete', guild => {
 console.log(`I have left ${guild.name} at ${new Date()}`);
 });
 
+var reload = (message, cmd) => {
+    delete require.cache[require.resolve('./commands/' + cmd)];
+    try {
+      let cmdFile = require('./commands/' + cmd);
+    } catch (err) {
+      message.channel.sendMessage(`Problem booting up ${cmd}: ${err}`).then(
+        response => response.delete(1000).catch(error => console.log(error.stack))
+      ).catch(error => console.log(error.stack));
+    }
+    message.channel.sendMessage(`${cmd} restart was a success :gordonyay:!`).then(
+      response => response.delete(1000).catch(error => console.log(error.stack))
+    ).catch(error => console.log(error.stack));
+  };
+  exports.reload = reload;
+  
 client.login(settings.token);
